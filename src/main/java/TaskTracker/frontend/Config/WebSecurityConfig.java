c2cb82d9c65835f1e0ec7996b8784b4f8350091c
Aleksander <evdokimov.aleksandr.2004@gmail.com>
2024-06-14 18:51:43 +0300

    Исправлена ошибка, приводящая к получению в сервис null значения

--- src/main/java/TaskTracker/frontend/Config/WebSecurityConfig.java
+++ src/main/java/TaskTracker/frontend/Config/WebSecurityConfig.java
@@ -4,9 +4,9 @@ import TaskTracker.businessLogic.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +15,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    UserServiceImpl userDetailService;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/", "/home", "/registration", "/css/**", "/icons/**", "/scripts/**").permitAll()
@@ -35,15 +38,14 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Autowired
-    UserServiceImpl userService;
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return userService;
+    public void configure(final AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(this.userDetailService)
+                .passwordEncoder(this.passwordEncoder());
     }
 
     @Bean
-    public PasswordEncoder encoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
